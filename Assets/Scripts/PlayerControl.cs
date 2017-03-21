4ff42040f8cf1276d6f206c264457f856d21f6f6
Rahul <patelr32@coventry.ac.uk>
2017-03-21 23:29:56 +0000

    New Project

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rigidBody;
+
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    //void Update ()
+    //{
+    //    transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10.13f, 9.95f), Mathf.Clamp(transform.position.y, -3.91f, 4.13f),transform.position.z);
+    //}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rigidBody.velocity = movement * speed;
+    }
+}
